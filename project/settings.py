c4af868e488620b92db4c61de8ae88668328083d
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2026-07-30 03:19:21 +0300

    add

--- project/settings.py
+++ project/settings.py
@@ -32,7 +32,7 @@ DEBUG = True  # ⚠️ خليها False في production
 
 ALLOWED_HOSTS = [
     # "mih-automated-pricing.up.railway.app",
-    # "web-production-8d018.up.railway.app",
+    "web-production-8d018.up.railway.app",
     "127.0.0.1",
     "localhost",
     ".railway.app",
@@ -42,7 +42,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     # "https://mih-automated-pricing.up.railway.app",
-    # "https://web-production-8d018.up.railway.app",
+    "https://web-production-8d018.up.railway.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-8d018.up.railway.app",
